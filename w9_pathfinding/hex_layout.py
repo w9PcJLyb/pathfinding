b3c6b2bcd2260028bc087408924fac08b8a67c57
w9PcJLyb <w9pcjlyb@icloud.com>
2025-06-04 10:33:06 +0400

    add docstring

--- w9_pathfinding/hex_layout.py
+++ w9_pathfinding/hex_layout.py
@@ -2,9 +2,92 @@ import enum
 
 
 class HexLayout(enum.IntEnum):
+    """
+    Enum representing hex grid layouts, defined by orientation and offset pattern.
+
+    Layout types:
+
+        'r' (row-based): pointy-top hexes aligned in rows
+        'q' (column-based): flat-top hexes aligned in columns
+        'odd': offset applies to odd rows or columns
+        'even': offset applies to even rows or columns
+
+    Members:
+
+        odd_r (0): Pointy-top hexes, odd rows are offset to the right
+
+           / \     / \     / \     / \
+         /     \ /     \ /     \ /     \
+        | (0,0) | (1,0) | (2,0) | (3,0) |
+        | (x,y) |       |       |       |
+         \     / \     / \     / \     / \
+           \ /     \ /     \ /     \ /     \
+            | (0,1) | (1,1) | (2,1) | (3,1) |
+            |       |       |       |       |
+           / \     / \     / \     / \     /
+         /     \ /     \ /     \ /     \ /
+        | (0,2) | (1,2) | (2,2) | (3,2) |
+        |       |       |       |       |
+         \     / \     / \     / \     /
+           \ /     \ /     \ /     \ /
+
+        even_r (1): Pointy-top hexes, even rows are offset to the right
+
+               / \     / \     / \     / \
+             /     \ /     \ /     \ /     \
+            | (0,0) | (1,0) | (2,0) | (3,0) |
+            | (x,y) |       |       |       |
+           / \     / \     / \     / \     /
+         /     \ /     \ /     \ /     \ /
+        | (0,1) | (1,1) | (2,1) | (3,1) |
+        |       |       |       |       |
+         \     / \     / \     / \     / \
+           \ /     \ /     \ /     \ /     \
+            | (0,2) | (1,2) | (2,2) | (3,2) |
+            |       |       |       |       |
+             \     / \     / \     / \     /
+               \ /     \ /     \ /     \ /
+
+        odd_q (2): Flat-top hexes, odd columns are offset downward
+           _ _           _ _
+         /     \       /     \
+        / (0,0) \ _ _ / (2,0) \ _ _
+        \ (x,y) /     \       /     \
+         \ _ _ / (1,0) \ _ _ / (3,0) \
+         /     \       /     \       /
+        / (0,1) \ _ _ / (2,1) \ _ _ /
+        \       /     \       /     \
+         \ _ _ / (1,1) \ _ _ / (3,1) \
+         /     \       /     \       /
+        / (0,2) \ _ _ / (2,2) \ _ _ /
+        \       /     \       /     \
+         \ _ _ / (1,2) \ _ _ / (3,2) \
+               \       /     \       /
+                \ _ _ /       \ _ _ /
+
+        even_q (3): Flat-top hexes, even columns are offset downward
+                  _ _           _ _
+                /     \       /     \
+           _ _ / (1,0) \ _ _ / (3,0) \
+         /     \       /     \       /
+        / (0,0) \ _ _ / (2,0) \ _ _ /
+        \ (x,y) /     \       /     \
+         \ _ _ / (1,1) \ _ _ / (3,1) \
+         /     \       /     \       /
+        / (0,1) \ _ _ / (2,1) \ _ _ /
+        \       /     \       /     \
+         \ _ _ / (1,2) \ _ _ / (3,2) \
+         /     \       /     \       /
+        / (0,2) \ _ _ / (2,2) \ _ _ /
+        \       /     \       /
+         \ _ _ /       \ _ _ /
+
+    """
+
     # pointy top
     odd_r = 0
     even_r = 1
+
     # flat top
     odd_q = 2
     even_q = 3
